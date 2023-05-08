0da9bcd9d435a9f1c74b9215d4be84113ddc9ae1
Peter Aleksander Bizjak <peteralexbizjak@protonmail.com>
2023-05-08 10:57:10 +0200

    Begin development of dart_ms

---
+++ dart_ms/README.md
@@ -0,0 +1,3 @@
+# dart_ms
+
+Millisecond conversion utility library inspired by [`ms`](https://www.npmjs.com/package/ms).
---
+++ dart_ms/lib/dart_ms.dart
@@ -0,0 +1,3 @@
+/// Millisecond conversion utility library inspired by
+/// [`ms`](https://www.npmjs.com/package/ms).
+library dart_ms;
