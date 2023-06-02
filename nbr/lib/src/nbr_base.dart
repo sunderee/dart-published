c890b2bb7898b1dc67ef9f832c7a662af45bdb1f
Peter Aleksander Bizjak <peteralexbizjak@protonmail.com>
2023-06-02 15:31:33 +0200

    Minor changes in the NBR itself

--- nbr/lib/src/nbr_base.dart
+++ nbr/lib/src/nbr_base.dart
@@ -8,11 +8,14 @@ import 'package:nbr/src/typedefs.dart';
 /// representing the current state of the resource (`loading`, `success`,
 /// `failed`, or `empty`).
 abstract class NetworkBoundResource<Entity> {
-  // The StreamController used to manage the stream of Resource objects.
-  final _controller = StreamController<Resource<Entity>>();
+  StreamController<Resource<Entity>>? _controller;
+  Stream<Resource<Entity>>? _stream;
 
   /// Getter for the stream of Resource objects.
-  Stream<Resource<Entity>> get stream => _controller.stream;
+  Stream<Resource<Entity>> get stream {
+    _ensureControllerCreated();
+    return _stream!;
+  }
 
   /// Method to fetch the resource from the network and emit [Resource] objects
   /// representing the current state of the resource.
@@ -28,33 +31,42 @@ abstract class NetworkBoundResource<Entity> {
     required ShouldFetchCallback<Entity> shouldFetch,
     required MapDTOToEntity<DTO, Entity> mapDTOToEntity,
   }) async {
-    _controller.add(Resource<Entity>.loading());
+    _controller?.add(Resource<Entity>.loading());
     final data = await loadFromDB();
 
     if (await shouldFetch(data)) {
-      _controller.add(Resource<Entity>.loading(data));
+      _controller?.add(Resource<Entity>.loading(data));
       try {
         final dto = await fetchFromAPI();
         final entity = mapDTOToEntity(dto);
 
         await storeToDB(entity);
 
-        _controller.add(Resource<Entity>.success(entity));
+        _controller?.add(Resource<Entity>.success(entity));
       } catch (exception) {
-        _controller.add(Resource<Entity>.failed(exception));
+        _controller?.add(Resource<Entity>.failed(exception));
       }
     } else {
       if (data != null) {
-        _controller.add(Resource<Entity>.success(data));
+        _controller?.add(Resource<Entity>.success(data));
       } else {
-        _controller.add(Resource<Entity>.empty());
+        _controller?.add(Resource<Entity>.empty());
       }
+    }
+  }
+
+  void _ensureControllerCreated() {
+    if (_controller == null) {
+      _controller = StreamController<Resource<Entity>>.broadcast();
+      _stream = _controller?.stream.asBroadcastStream();
     }
   }
 
   /// Method to dispose of the [NetworkBoundResource] by closing the
   /// [StreamController].
   void dispose() {
-    _controller.close();
+    _controller?.close();
+    _controller = null;
+    _stream = null;
   }
 }
